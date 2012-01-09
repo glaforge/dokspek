deaa8eb05f096c3ec5e31d6cb54617e4cde5a32c
Guillaume Laforge <glaforge@gmail.com>
2012-01-09 12:49:18 +0100

    Sample test used to try to fail the build when errors happen in the embedded scripts (the test will later be removed once the lack of build failure and JUnit report lack of failure mention is resolved)

---
+++ src/test/groovy/dokspek/SampleTest.groovy
@@ -0,0 +1,12 @@
+package dokspek
+
+import org.junit.runner.RunWith
+import dokspek.junit.DokspekRunner
+
+/**
+ * 
+ * @author Guillaume Laforge
+ */
+@RunWith(DokspekRunner)
+class SampleTest {
+}
